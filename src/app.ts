3e55ef9989ce61abea7f0bd93ca332d44bfe97dd
kumarbibek42 <kumarbibek42@gmail.com>
2020-01-31 00:40:58 +0530

    Remove newLine

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,6 @@ class App {
   private setConfig() {
     //Allows us to receive requests with data in json format
     this.app.use(bodyParser.json({ limit: '50mb' }));
-
     //Allows us to receive requests with data in x-www-form-urlencoded format
     this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
     //Enables cors
